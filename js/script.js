32910abfb25fa7d1872007c2ba7aaa4993de677c
MYuPopov <beemax@bk.ru>
2022-01-05 22:10:28 +0300

    Усложненное задание урока 26

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ const sendData = (data) => {
 	})
 		.then((res) => res.json())
 		.then((data) => {
-			console.log(error);
+			console.log(data);
 		})
 		.catch((error) => {
 			console.log(error);
@@ -30,3 +30,28 @@ const sendData = (data) => {
 };
 
 sendData(getData());
+
+/* Усложненное задание */
+/* XMLHttpRequest */
+const xhrGet = new XMLHttpRequest();
+xhrGet.open('GET', 'db.json', [false]);
+xhrGet.send();
+xhrGet.onload = function () {
+	let response = xhrGet.response;
+	response = JSON.stringify(response);
+	// Убрать все символы: \r\n \ и пробелы в 4 знака
+	// потом сделать JSON.parse()
+	console.log(response);
+	return response;
+};
+
+let xhrSend = new XMLHttpRequest();
+xhrSend.open('POST', 'https://jsonplaceholder.typicode.com/posts', [false]);
+xhrSend.send(xhrGet.onload());
+xhrSend.onload = function () {
+	let response = xhrSend.response;
+	response = JSON.stringify(response);
+	// Убрать все символы: \r\n \ и пробелы в 4 знака
+	// потом сделать JSON.parse()
+	console.log(response);
+};
